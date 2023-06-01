1d39afc49b524faae05a66806bf3c3d552ce54d4
Rohith172Reddy <rohithg7410@gmail.com>
2023-05-31 20:31:03 -0500

    adding files

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        // Set the name using the setName() method
+        student.setName("Rohith");
+
+        // Get the name using the getName() method
+        String name = student.getName();
+        System.out.println("Name: " + name);
+    }
+}
+
