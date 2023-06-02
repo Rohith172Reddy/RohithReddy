93aac6f7b81c03f649dd210370f4ec38f9e7754e
Rohith172Reddy <rohithg7410@gmail.com>
2023-06-02 15:02:29 -0500

    First commit

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    double price;
+    public double getPrice() {
+        return price = 4;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice(int quantity) {
+        return price * quantity;
+    }
+}
